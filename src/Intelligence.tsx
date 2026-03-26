81fc5ac21734cfb310e697f6a4fe51257ba123ce
Jonas Hyltén <jonasthevathason@MacBook-Pro-2.local>
2026-03-26 01:34:46 +0100

    Add discreet LinkedIn icon to article pages

--- src/Intelligence.tsx
+++ src/Intelligence.tsx
@@ -273,6 +273,18 @@ export const IntelligenceArticle: React.FC<{
 
   return (
     <div className="pt-32 pb-20 px-8 md:px-24 max-w-4xl mx-auto">
+      {/* LinkedIn Personal - Ultra Discreet */}
+      <a
+        href="https://www.linkedin.com/in/hylten/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-6 right-6 z-[60] opacity-20 hover:opacity-60 transition-all duration-300"
+      >
+        <svg className="w-3 h-3 text-white/50" fill="currentColor" viewBox="0 0 24 24">
+          <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+        </svg>
+      </a>
+
       <Helmet>
         <title>{article.title} | Venture Studio Intelligence</title>
         <meta name="description" content={article.description} />
